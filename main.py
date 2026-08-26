ee75e04e6d94539a3b69f39a71da09f6fe99847e
Nikos Chalatsis <Nik_chal@live.com>
2026-08-26 14:10:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -79,23 +79,23 @@ class MediaCatalogue:
                 result += f"{i}. {show}\n"
         
         return result
-
-catalogue = MediaCatalogue()
-
-try:
-    movie1 = Movie('The Matrix', 1999, 'The Wachowskis', 136)
-    catalogue.add(movie1)
-    movie2 = Movie('Inception', 2010, 'Christopher Nolan', 148)
-    catalogue.add(movie2)
+if __name__ == "__main__":
+    catalogue = MediaCatalogue()
     
-    series1 = TVSeries('Scrubs', 2001, 'Bill Lawrence', 24, 9, 182)
-    catalogue.add(series1)
-    series2 = TVSeries('Breaking Bad', 2008, 'Vince Gilligan', 47, 5, 62)
-    catalogue.add(series2)
-    
-    print(catalogue)
-except ValueError as e:
-    print(f'Validation Error: {e}')
-except MediaError as e:
-    print(f'Media Error: {e}')
-    print(f'Unable to add {e.obj}: {type(e.obj)}')
+    try:
+        movie1 = Movie('The Matrix', 1999, 'The Wachowskis', 136)
+        catalogue.add(movie1)
+        movie2 = Movie('Inception', 2010, 'Christopher Nolan', 148)
+        catalogue.add(movie2)
+        
+        series1 = TVSeries('Scrubs', 2001, 'Bill Lawrence', 24, 9, 182)
+        catalogue.add(series1)
+        series2 = TVSeries('Breaking Bad', 2008, 'Vince Gilligan', 47, 5, 62)
+        catalogue.add(series2)
+        
+        print(catalogue)
+    except ValueError as e:
+        print(f'Validation Error: {e}')
+    except MediaError as e:
+        print(f'Media Error: {e}')
+        print(f'Unable to add {e.obj}: {type(e.obj)}')
